7e1461b8c9ba3ca5cff830a3940871ff9572268e
Angel Rosales Contreras <angel.r.c.utt.4@gmail.com>
2023-09-06 22:11:25 -0600

    un vuh

--- test1.py
+++ test1.py
@@ -1,16 +1,3 @@
-print("Cambio A")
-print("dddd")
-print("333")
-print("cambio c")
-print("dwdwdw")
-print("Cambio Nuevo")
-print("sasasa")
-print("seqwdqdqdqdqdxxxxxqdqdqdqdq")
-print("Hswsw")
-print("e32edww")
-print("ee2e2")
-print("Cambio A")
-print("sasa")
-print("dddd")
-print("333")
-print("cambio c")
+def uno(x,y):
+    su = x+y
+    return su
